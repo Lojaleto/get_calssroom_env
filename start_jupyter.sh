ee1fe723670991e79e874e9ae49a8f4ba2a0cf1a
Lojaleto <yes@loj.me>
2023-02-10 19:58:10 +0400

    up

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -14,29 +14,34 @@ while true; do
     fi
 done
 
-nohup docker exec $miniconda /opt/conda/bin/conda run -n $env --no-capture-output jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root > ./jupyter.log &
-
-sleep 5
-while true; do
-    lsize="$(wc -c ./jupyter.log | awk '{ print $1 }')"
-    if [[ $lsize > 1000 ]]; then
-        cat ./jupyter.log
-        echo 'other ip:'
-        ip route get 8.8.8.8 |awk '{ print $7 }'
-        echo '' > ./jupyter.log
-        break;
-    else sleep 3; echo "jupyter is not running"
-    fi
-done
 
 while true; do
-    echo $jpid
-    read -p "Do you wish to S top miniconda or R estart jupyter?  " yn
-    case $yn in
-        [Ss]* ) docker stop $miniconda; break;;
-        [Rr]* ) pkill -f jupyter; nohup docker exec $miniconda /opt/conda/bin/conda run -n $env --no-capture-output jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root > ./jupyter.log &;;
-        * ) echo "Please answer S to close or R to restart";;
-    esac
+
+    nohup docker exec $miniconda /opt/conda/bin/conda run -n $env --no-capture-output jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root > ./jupyter.log &
+
+    sleep 5
+    while true; do
+        lsize="$(wc -c ./jupyter.log | awk '{ print $1 }')"
+        if [[ $lsize > 1000 ]]; then
+            cat ./jupyter.log
+            echo 'other ip:'
+            ip route get 8.8.8.8 |awk '{ print $7 }'
+            echo '' > ./jupyter.log
+            break;
+        else sleep 3; echo "jupyter is not running"
+        fi
+    done
+
+    while true; do
+        echo $jpid
+        read -p "Do you wish to S top miniconda or R estart jupyter?  " yn
+        case $yn in
+            [Ss]* ) docker stop $miniconda; exit;;
+            [Rr]* ) pkill -f jupyter;;
+            * ) echo "Please answer S to close or R to restart";;
+        esac
+    done
+
 done
 
 exit;
