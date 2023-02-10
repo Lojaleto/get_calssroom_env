be0c9585632523616ac8c7c1c3d6113a202382c3
Lojaleto <yes@loj.me>
2023-02-10 14:21:31 +0400

    up

--- jup_env_create.sh
+++ jup_env_create.sh
@@ -17,7 +17,7 @@ docker exec $env apt update &> /dev/null
 docker exec $env apt install -y wget
 docker exec $env wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh &> /dev/null
 docker exec $env bash ./Miniconda3-latest-Linux-x86_64.sh -b -p /opt/conda
-docker exec $env wget "http://85.113.39.210/classroom.yml" &> /dev/null
+docker exec $env wget "https://raw.githubusercontent.com/Lojaleto/get_calssroom_env/main/classroom.yml" &> /dev/null
 docker exec $env /opt/conda/bin/conda env create -f ./classroom.yml
 docker exec $env /opt/conda/bin/conda activate classroom
 docker exec $env /opt/conda/bin/conda install jupyter -y --quiet
